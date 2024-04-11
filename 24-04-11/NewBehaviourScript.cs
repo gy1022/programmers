7fd47c98e6bc55781b4261273f96efadeb23b7d7
gy1022 <bokowife2@naver.com>
2024-04-11 17:13:54 +0900

    [Feat]문자열 출력하기

---
+++ 24-04-11/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        String s;
+
+        Console.Clear();
+        s = Console.ReadLine();
+        Console.WriteLine(s);
+
+    }
+}
+
