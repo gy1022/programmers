4bb73aa4dc7e5ea45ba7255a9ed30918f465f2c0
gy1022 <bokowife2@naver.com>
2024-04-15 16:12:07 +0900

    [Feat]특수문자 출력하기

---
+++ 24-04-15 - 2/NewBehaviourScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // 주어진 출력 예시대로 문자열을 출력합니다.
+        // 이스케이프 문자를 사용하여 특수 문자를 출력합니다.
+        Console.WriteLine("!@#$%^&*(\\'\"<>?:;");
+    }
+}
