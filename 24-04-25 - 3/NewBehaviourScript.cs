8289dc1ab9a24bf977c073c4533faf6ef20a9d00
gy1022 <bokowife2@naver.com>
2024-04-25 11:49:17 +0900

    [Feat] 수 조작하기 1

---
+++ 24-04-25 - 3/NewBehaviourScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Solution
+{
+    public int solution(int n, string control)
+    {
+        int answer = n;
+        for (int i = 0; i < control.Length; i++)
+        {
+            switch (control[i])
+            {
+                case 'w':
+                    answer += 1;
+                    break;
+                case 's':
+                    answer -= 1;
+                    break;
+                case 'd':
+                    answer += 10;
+                    break;
+                case 'a':
+                    answer -= 10;
+                    break;
+            }
+        }
+        return answer;
+    }
+}
